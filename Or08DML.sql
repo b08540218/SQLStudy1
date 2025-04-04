6a5b79a0a67c743bf8e0c0084af7e87ba8140503
b08540218 <b08540218@gmail.com>
2025-04-04 17:50:08 +0900

    2025년04월04일 커밋 17:50

--- Or08DML.sql
+++ Or08DML.sql
@@ -41,21 +41,72 @@ SELECT * from tb_sample;
 컬럼을 지정해서 insert 하는 경우 데이터를 삽입하지 않을 컬럼을 지정할 수
 있다. 아래의 경우 name컬럼은 null값이 삽입된다.
 */
-insert INTO tb_sample (no, loc, manager) 
-    VALUES (50, '제주', '동탁');
+insert INTO tb_sample (no, loc, manager) VALUES (50, '제주', '동탁');
 SELECT * from tb_sample;
 /*
 지금까지의 작업을 그대로 유지하겠다는 명령으로 커밋을 수행하지 않으면
 오라클 외부에서는 변경된 레코드를 확인할 수 없다.
 여기서 말하는 외부란 Java/JSP와 같은 Oracle 이외의 프로그램을 말한다.
 */
-commit; --확인
---커밋 이후 새로운 레코들 삽입하면 임시데이터를 저장한다.
-insert INTO tb_sample (no, loc, manager) 
-    VALUES (60, '태국', '손오공');
+commit;
+
+--커밋 이후 새로운 레코드를 삽입하면 임시데이터를 저장한다.
+insert INTO tb_sample (no, loc, manager) VALUES (60, '태국', '손오공');
 --select 명령으로 확인할 수 있지만 실제 테이블에는 반영되지 않은 상태이다.
 SELECT * from tb_sample;
 --롤백 명령으로 마지막 커밋 상태로 되돌릴 수 있다.
 rollback;
 --마지막 커밋 이후에 삽입된 '손오공'은 제거된다.
 SELECT * FROM tb_sample;
+
+
+/*
+레코드 수정하기 : update
+형식] update 테이블명 set 컬럼=값  where 조건;
+※조건이 없는경우 모든 레코드가 한꺼번에 수정된다.
+※테이블명 앞에 from이 들어가지 않는다.
+*/
+--번호가 40인 레코드인 지역을 '미국'으로 수정하시오
+UPDATE tb_sample set loc='미국' WHERE no=40;
+SELECT * FROM tb_sample;
+--지역이 서울인 레코드를 메니져명을 '제갈공명'으로 변경하시오
+UPDATE tb_sample set manager='제갈공명' WHERE loc='서울';
+SELECT * FROM tb_sample;
+
+--모든 레코드를 대상으로 지역을'가디'로 변경하시오.
+UPDATE tb_sample set loc='가디';
+--전체 레코드를 대상으로 하는 경우 where절을 생략하면 된다.
+SELECT * FROM tb_sample;
+
+/*
+레코드 삭제하기 : delete
+형식] delete from 테이블명 where 조건;
+※레코드를 삭제하므로 delete 뒤에 컬럼을 명시하지 않는다.
+*/
+--번호 10인 레코드를 삭제하시오.
+delete from tb_sample where no=10;
+SELECT * FROM tb_sample;
+--레코드 전체를 삭제하시오.
+delete FROM tb_sample;
+SELECT * FROM tb_sample;
+--마지막에 커밋했던 지점으로 되돌린다.
+ROLLBACK;
+SELECT * FROM tb_sample;
+
+/*
+DDL문 : 테이블을 생성 및 조작하는 쿼리문
+    (Data Definition Language : 데이터 정의어)
+    테이블 생성 : create table 테이블명
+    테이블 수정
+        컬럼추가 : alter table 테이블명 add 컬럼명
+        컬럼수정 : alter table 테이블명 modify 컬럼명
+        컬럼삭제 : alter table 테이블명 drop column 컬럼명
+    테이블 삭제 : drop table 테이블명 
+    
+DML문 : 레코드를 입력 및 조작하는 쿼리문
+    (Data Manipulation Language : 데이터 조작어)
+    레코드 입력 : insert into 테이블명 (컬럼명) values (값)
+    레코드 수정 : update 테이블명 set 컬럼=값 where 조건
+    레코드 삭제 : delete from 테이블명 where 조건
+    
+*/
